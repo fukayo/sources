2eed6cada590b9bb80d96e4d8537fc2d82cbb312
Jean-Philippe ALLEGRO <26584973+JiPaix@users.noreply.github.com>
2023-06-10 23:00:30 +0200

    ci(release): fix releases

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const hack = import.meta.glob(['./*.ts', '!./*.d.ts'], { eager: true })
+import { writeFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const FN = fileURLToPath(import.meta.url)
+
+const DIRNAME = dirname(FN)
+export const hack = import.meta.glob(['./*.ts', './*.js', '!./*.d.ts'], { eager: true })
+
+function getVersion (input: unknown): number {
+  console.log(input)
+  return (input as { version: number }).version
+}
+
+function getId (input: string): string {
+  return input.replace(/^\.\//, '').replace(/\.js$|\.ts$/, '')
+}
+
+const all = Object
+  .keys(hack)
+  .map(key => Object.assign({}, { id: getId(key), version: getVersion(hack[key]) }))
+
+writeFileSync(resolve(DIRNAME, 'all.json'), JSON.stringify(all))
