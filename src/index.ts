822e3dc5ec20610f9b3e773d8b4b37ef8afb2281
Jean-Philippe ALLEGRO <26584973+JiPaix@users.noreply.github.com>
2023-06-11 00:05:34 +0200

    ci(release): add versions to all.json artifact

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { rmSync, writeFileSync } from 'fs'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
-
+const args = process.argv.slice(2)
+if (!args[0]?.startsWith('v')) throw Error(`bad tag:${args}`)
 const FILENAME = fileURLToPath(import.meta.url)
 
 const DIRNAME = dirname(FILENAME)
 export const hack = import.meta.glob(['./*.ts', './*.js', '!./*.d.ts'], { eager: true })
 
 function getVersion (input: unknown): number {
-  console.log(input)
   return (input as { version: number }).version
 }
 
@@ -18,7 +18,7 @@ function getId (input: string): string {
 
 const all = Object
   .keys(hack)
-  .map(key => Object.assign({}, { id: getId(key), version: getVersion(hack[key]) }))
+  .map(key => Object.assign({}, { id: getId(key), version: getVersion(hack[key]), url: `https://github.com/fukayo/sources/releases/download/${args[0]}/all.json` }))
 
 writeFileSync(resolve(DIRNAME, 'all.json'), JSON.stringify(all))
 rmSync(FILENAME)
