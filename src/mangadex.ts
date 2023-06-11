4817ffb51239fd66ea00b0e149fbe3c568a52dc3
Jean-Philippe ALLEGRO <26584973+JiPaix@users.noreply.github.com>
2023-06-11 04:29:25 +0200

    fix(mangadex): crawler wasn't initd by source

--- src/mangadex.ts
+++ src/mangadex.ts
@@ -1,6 +1,6 @@
 
 import { type SourceResponse, type SourceError } from './interface/shared.js'
-import type { CrawlerInstance, Source } from './interface/abstract.js'
+import type { Crawler, CrawlerInstance, Source } from './interface/abstract.js'
 
 const settings = {
   version: 0,
@@ -11,18 +11,23 @@ const settings = {
 class Mangadex implements Source {
   static #instance: Mangadex
   #scrapper?: CrawlerInstance
+  #baseURL = 'https://api.mangadex.org'
 
-  static async getInstance (crawler: CrawlerInstance): Promise<Mangadex> {
+  static async getInstance (crawler: Crawler): Promise<Mangadex> {
     if (!(this.#instance instanceof Mangadex)) {
       this.#instance = new this()
-      this.#instance.#scrapper = crawler
+      this.#instance.#scrapper = await crawler.getInstance({
+        matches: [new URL(this.#instance.#baseURL).host],
+        points: 5,
+        duration: 1000
+      })
     }
     return this.#instance
   }
 
   async search<T> (query: string): Promise<SourceResponse<T> | SourceError> {
     if (!this.#scrapper) return { success: false, message: 'init_error' }
-    const resp = await this.#scrapper?.getCrawler('https://api.mangadex.org/manga', 'json', { params: { title: query }, skipCache: { from: true, to: true } })
+    const resp = await this.#scrapper.getCrawler('https://api.mangadex.org/manga', 'json', { params: { title: query }, skipCache: { from: true, to: true } })
     return {
       success: true,
       data: resp as T
